1df1d8822cb27b22f031131c6b4b728e8fc4e65b
pengpengmi <415796161@qq.com>
2021-01-19 00:24:13 +0800

    leetcode 高频

---
+++ src/main/java/com/leetcode/Problem_0017_LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,12 @@
+package com.leetcode;
+
+/**
+ * 2-9数字上映射的英文字母
+ * 给定两个数，输出数字中包含的所有的两个字母的组合
+ * 如 输入2，3
+ * 输出 【“ad”，“ae”，“af”，“bd”，“be”，“bf”，“dc”,"ce","cf"】
+ * 暴力递归
+ */
+public class Problem_0017_LetterCombinationsOfAPhoneNumber {
+
+}
